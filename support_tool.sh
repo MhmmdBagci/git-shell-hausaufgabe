90cd056bd518e2a91935b36fb45d33103869741b
Muhammed Bagci <muhammedabdullaharun@gmail.com>
2026-05-26 12:50:29 +0200

    Einfaches Menue hinzugefuegt

--- support_tool.sh
+++ support_tool.sh
@@ -11,3 +11,15 @@ echo "Bitte geben Sie Ihren Namen ein:"
 read name 
 
 echo "Hallo $name, willkommen im IT Support Tool." 
+
+echo "" 
+
+echo "Bitte wählen Sie eine Aufgabe:" 
+
+echo "1) Systeminformationen anzeigen" 
+
+echo "2) Speicherplatz prüfen" 
+
+echo "3) Programm beenden" 
+
+read auswahl 
